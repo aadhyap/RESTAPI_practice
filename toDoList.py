20d043678a55f146e176aa6036a5ff720fb4311e
aadhyaputtur <putturaadhya@gmail.com>
2023-10-24 07:42:57 -0400

    Implement Rate Limiting
    Implementing a method to prevent a client with a unique IP address to create too many incoming requests in a given time

--- toDoList.py
+++ toDoList.py
@@ -1,9 +1,15 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request, abort, make_response
+
+from time import time
+
+from functools import wraps
 
 
 app = Flask(__name__)
 tasks = []
 task_id = 1
+
+client_times ={}
 
 @app.route('/tasks', methods=['Get'])
 
@@ -14,32 +20,70 @@ def get_tasks():
   paginated_tasks = tasks[offset:offset+limit]
   return jsonify(tasks)
 
+#implement authentication
+def check_auth(username, password):
+  return username =="admin" and password=="password"
+
+
+def requires_auth(f):
+  @wraps(f)
+  
+  def decorated(*args, **kwargs):
+    auth = request.authorization
+    if not auth or not check_auth(auth.username, auth.password):
+      abort(401)
+    return f(*args, **kwargs)
+  return decorated
+
+@app.route('/tasks', methods=['POST', 'PUT', 'DELETE'])
+@requires_auth
+
+
+def rate_limit(f):
+  @wraps(f)
+
+  def decorated(*args, **kwargs):
+    client_ip = request.remote_addr
+    current_time = int(time())
+    requests = client_times.get(client_ip, [])
+
+    #Filter requests in the last minute
+    requests = [req_time for req_time in requests if current_time - req_time < 60]
+
+    if len(requests) >= 10:
+      return make_response(jsonify({"error":"Too many requests"}), 429)
+
+    requests.append(current_time)
+    client_times[client_ip] = requests
+    return f(*args, **kwargs)
+  return decorated
 
 @app.route('/tasks', methods=['POST'])
 def add_task():
-  global task_id
-  new_task = {"id": task_id, "task": request.json['task']}
-  tasks.append(new_task)
-  task_id += 1
+    global task_id
+    new_task = {"id": task_id, "task": request.json['task']}
+    tasks.append(new_task)
+    task_id += 1
 
-  return jsonify(new_task), 201
+    return jsonify(new_task), 201
 
 
 @app.route('/tasks/<int:id>', methods=['PUT'])
 
 def update_task(id):
-  task = next((item for item in tasks if item['id'] == id), None)
-  if task is None:
-    return jsonify({"error":"Task not found"}), 404
+    task = next((item for item in tasks if item['id'] == id), None)
+    if task is None:
+      return jsonify({"error":"Task not found"}), 404
 
-  task['task'] = request.json['task']
-  return jsonify(task)
+    task['task'] = request.json['task']
+    return jsonify(task)
 
 @app.toute('/tasks/<int:id>', methods=['DELETE'])
 
 def delete_task(id):
-  global id
-  tasks = [task for task in tasks if task['id'] != id]
+    global tasks
+    tasks = [task for task in tasks if task['id'] != id]
 
-  return jsonify({"result":"Task deleted"})
+    return jsonify({"result":"Task deleted"})
+
 
